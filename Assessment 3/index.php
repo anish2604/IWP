1a52c2e8441a91e286762c2a0c6ab319d642fd0a
anish2604 <anishfiitjee1719@gmail.com>
2022-03-04 23:57:39 +0530

    Assessment 3 added

---
+++ Assessment 3/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h2>Personal Details</h2>
+        </div>
+        <div class="form" id="form" action="" method="get">
+            <div class="form-control">
+                <p class="info">This application is use to validate user Registration Credentiala using HTML, CSS and
+                    Javascript, and display suitable messages, accordingly.
+                </p>
+                <label>Name: Anish Chattaraj</label>
+                <label>Registration No.: 19BCE0884</label>
+            </div>
+            <div class="buttons">
+                <a href="register.php" class="links">Signup</a>
+            </div>
+            </form>
+        </div>
+
+        <script src="index.js"></script>
+
+</body>
+
+</html>
